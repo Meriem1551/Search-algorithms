6e7d5886bb07f72a10973c0253205aa7b48acc3d
Meriem1551 <meriem200409@gmail.com>
2023-10-04 06:32:52 +0200

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
     int size = get_input("Size: ");
     int *arr = get_Array("Enter the array", size);
     int num = get_input("Searching the number: ");
-    // printf("%d", Sequencial_search(arr, size, num));
+    printf("%d", Sequencial_search(arr, size, num));
     printf("%d", Dichotomous_search(arr, size, num));
     return 0;
 }

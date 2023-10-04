2b778e83f18329ff08bebf8fc5bb85b29973b762
Meriem1551 <meriem200409@gmail.com>
2023-10-04 06:23:09 +0200

    dichotomous searche

---
+++ dic_search.c
@@ -0,0 +1,28 @@
+// we mast use a sorting array
+
+#include "main.h"
+
+int Dichotomous_search(int T[], int size, int x)
+{
+    int min = 0, max = size - 1, med;
+    do
+    {
+        med = (max + min) / 2;
+        if (T[med] == x)
+        {
+            return 1;
+        }
+        else
+        {
+            if (T[med] < x)
+            {
+                min = med + 1;
+            }
+            else
+            {
+                max = med - 1;
+            }
+        }
+    } while (min < max);
+    return -1;
+}

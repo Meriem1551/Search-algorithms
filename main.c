b909bf881537b1418a4067be85f34be75e7ec946
Meriem1551 <meriem200409@gmail.com>
2023-10-05 18:41:08 +0200

    Update

--- main.c
+++ main.c
@@ -3,10 +3,14 @@
 #include <stdlib.h>
 int main()
 {
+
     int size = get_input("Size: ");
     int *arr = get_Array("Enter the array", size);
     int num = get_input("Searching the number: ");
-    printf("%d", Sequencial_search(arr, size, num));
-    printf("%d", Dichotomous_search(arr, size, num));
+    int choice = get_input("Enter 1 for sequencial and 2 for dichotomous: ");
+    if (choice == 1)
+        printf("The result by using seq: %d", Sequencial_search(arr, size, num));
+    else
+        printf("The result by usinf dic is: %d", Dichotomous_search(arr, size, num));
     return 0;
 }
